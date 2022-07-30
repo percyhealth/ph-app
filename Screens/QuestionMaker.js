a22666c5816e7ba6f743175c6524e992acfab2fc
John Pumayalli <johngenopuma@gmail.com>
2022-07-30 12:55:58 -0600

    working  questionmaker

--- Screens/QuestionMaker.js
+++ Screens/QuestionMaker.js
@@ -1,131 +1,176 @@
 import React, {useState} from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TouchableHighlight, } from 'react-native';
-const Separator = () => (
-  <View style={styles.separator} />
-);
-
-var boolList = [];
-var place = 0;
-function overAll(qList){
-  function EachButton(string, theList, boolList2){
-    const [response, setResponse] = useState ("Not Selected");
-    let length = theList.length;
-    for(var i = 0; i < length; i++){
-      if(string == theList[i]){
-        break;
-      }else{
-        place++;
-      }
-    }
-    function bStyle(){
-      if((boolList2[place])){
-        //response = "true";
-        return(styles.buttonStyleOn);
-      }else if (!(boolList2[place])){
-        //response = "false";
-        return(styles.buttonStyleOff);
-      }
-    }
-    function pressFunc(){
-      if(place == 0){
-        setResponse(string);
-        Alert.alert("first");
-      }else if(place == 1){
-        setResponse(string);
-        Alert.alert("second");
-      }
-    };
-    
-    return(
-      <View>
-        <View style = {bStyle()}>
-          <Text>{boolList[place].toString()}</Text>
-          <Text>{place}</Text>
-          <Text>{response}</Text>
-          <Button 
-            title= {string} 
-            color = "black"
-            onPress={() => pressFunc()}
-          />
-        </View>
-        <Separator/>
-      </View>
-      
-    );
-  }
-  function QRender(){
-    const types = qList;
-    for (var i = 0; i < types.length; i++){
-      boolList.push(false);
-    }
-    boolList[0] = true;
-    return(
-      <View>
-        {types.map(type=>(
-          EachButton(type, qList, boolList)
-        ))}
-      </View>
-    )
-  }
-  return(QRender());
+import {
+ StyleSheet,
+ Button,
+ View,
+ SafeAreaView,
+ Text,
+ Alert,
+ TouchableHighlight,
+} from 'react-native';
+import { isSearchBarAvailableForCurrentPlatform } from 'react-native-screens';
+const Separator = () => <View style={styles.separator} />;
+ 
+function overAll(qList, mainTitle, subTitle) {
+ function resetList(){
+   paraList = [];
+   for (var i = 0; i < qList.length; i++) {
+     paraList.push(false);
+   }
+   return(paraList);
+ }
+ const [boolList, setBoolList] = useState(resetList());
+ function EachButton(string, theList) {
+   tempBool = [];
+   tempBool = resetList();
+   var place = 0;
+   let length = theList.length;
+   for (var i = 0; i < length; i++) {
+     if (string == theList[i]) {
+       break;
+     } else {
+       place++;
+     }
+   }
+   function bStyle() {
+     if(boolList[place]){
+       return (styles.buttonStyleOn)
+     }else if(!boolList[place]){
+       return (styles.buttonStyleOff)
+     }
+   }
+   function bTxtColor(){
+     if(boolList[place]){
+       return ('white');
+     }else if(!boolList[place]){
+       return ('black');
+     }
+   }
+   function pressFunc() {
+     tempBool  = resetList();
+     tempBool[place] = !boolList[place];
+     setBoolList(tempBool);
+   }
+ 
+   return (
+     <View>
+       <View style={bStyle()}>
+         <Button
+         title={string}
+         color={bTxtColor()}
+         onPress={() => pressFunc()}
+         />
+       </View>
+       <Separator />
+     </View>
+   );
+ }
+ function optionSelected(){
+   var Selected = false;
+   for(var i = 0; i< boolList.length;i++){
+     if(boolList[i]){
+       Selected = true;
+     }
+   }
+   return(Selected);
+ }
+ function togNext(){
+   var opSelected = optionSelected();
+   if(opSelected){
+     Alert.alert('You May Pass');
+   }else{
+     Alert.alert('Please select a response');
+   }
+ }
+ function continueBStyle(){
+   var opSelected = optionSelected();
+   if(opSelected){
+     return(styles.buttonStyleOn);
+   }else{
+     return(styles.buttonStyleOff);
+   }
+ }
+ function continueBColor(){
+   var opSelected = optionSelected();
+   if(opSelected){
+     return('white');
+   }else{
+     return('black');
+   }
+ }
+ function QRender(){
+   return (
+     <View>
+       {/* <Text style={styles.head_title}>
+         {subTitle}
+       </Text> */}
+       {subTitle}
+       <Text style={styles.title}>
+         {mainTitle}
+       </Text>
+       {qList.map(type => EachButton(type, qList))}
+       <View style={styles.filler} />
+       <View style = {continueBStyle()}>
+         <Button
+         title = "Continue"
+         color = {continueBColor()}
+         onPress = {() => togNext()}
+         />
+       </View>
+     </View>
+   );
+ }
+ return QRender();
 }
-
-
+ 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'flex-start',
-    marginHorizontal: 16,
-    marginVertical: 50,
-  },
-  head_title: {
-    //fontFamily: "SF Pro Display",
-    fontSize: 17,
-    textAlign: 'left',
-    opacity: .9, 
-  },
-  title_bold:{
-    fontSize: 17,
-    textAlign: 'left',
-    opacity: .9, 
-    fontWeight: "bold"
-  },
-  title: {
-    textAlign: 'left',
-    marginVertical: 16,
-    //fontFamily: "SF Pro Display",
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    fontSize: 22,
-    lineHeight: 26,
-    display: 'flex',
-    alignItems: 'center',
-    letterSpacing: 0.35,
-    color: '#212121',
-    opacity: 0.9
-  },
-  buttonStyleOn: {
-    backgroundColor: '#007AFF',
-    borderColor: '#007AFF',
-    //borderColor: 'black',
-    borderRadius: 10,
-    borderWidth: 1,
-  },
-  buttonStyleOff: {
-    backgroundColor: 'white',
-    borderColor: 'black',
-    borderRadius: 10,
-    borderWidth: 1,
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: 'white',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  filler: {
-    marginVertical: 100,
-  }
-  
+ container: {
+   flex: 1,
+   // justifyContent: 'flex-start',
+   marginHorizontal: 16,
+   marginVertical: 50,
+ },
+ head_title: {
+   //fontFamily: "SF Pro Display",
+   fontSize: 17,
+   textAlign: 'left',
+   opacity: 0.9,
+ },
+ title: {
+   textAlign: 'left',
+   marginVertical: 16,
+   //fontFamily: "SF Pro Display",
+   fontStyle: 'normal',
+   fontWeight: 'normal',
+   fontSize: 22,
+   lineHeight: 26,
+   display: 'flex',
+   alignItems: 'center',
+   letterSpacing: 0.35,
+   color: '#212121',
+   opacity: 0.9,
+ },
+ buttonStyleOn: {
+   backgroundColor: '#007AFF',
+   borderColor: '#007AFF',
+   //borderColor: 'black',
+   borderRadius: 10,
+   borderWidth: 1,
+ },
+ buttonStyleOff: {
+   backgroundColor: 'white',
+   borderColor: 'black',
+   borderRadius: 10,
+   borderWidth: 1,
+ },
+ separator: {
+   marginVertical: 8,
+   borderBottomColor: 'white',
+   borderBottomWidth: StyleSheet.hairlineWidth,
+ },
+ filler: {
+   marginVertical: 92,
+ },
 });
-
+ 
 export default overAll;
--- Screens/thirdScreen.js
+++ Screens/thirdScreen.js
@@ -1,25 +1,61 @@
 import React, {useState} from 'react';
 import QRender from './QuestionMaker';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TouchableHighlight, } from 'react-native';
+import {
+ StyleSheet,
+ Button,
+ View,
+ SafeAreaView,
+ Text,
+ Alert,
+ TouchableHighlight,
+} from 'react-native';
 //import {Text} from 'react-native';
-const Separator = () => (
-  <View style={styles.separator} />
-);
-
+ 
+// commenting out bc unsued and style.separator causing an error
+// const Separator = () => <View style={styles.separator} />;
+ 
 const ThirdScreenFunc = ({navigation}) => {
+ const subTitle = (
+ <Text style={styles.head_title}>
+   How does <Text style={styles.title_bold}>your health limit you</Text>{' '}
+   in
+ </Text>);
+ const mainTitle = "Moderate activities, such as moving a table, pushing a vacuum cleaner bowling, or playing golf";
+ const types = ['Yes, limited a lot', 'Yes, limited a little', 'No, not limited at all'];
+ function TheRendered() {
+   return QRender(types, mainTitle, subTitle);
+ }
+ return (
+   <SafeAreaView style= {styles.container}>
+     <View>
+       <TheRendered/>
+     </View>
+   </SafeAreaView>
   
-  const types = ["Laurel", "John"]
-  function TheRendered(){
-    return(
-      QRender(types)
-    );
-  }
-  return(
-    <View>
-      <TheRendered/>
-      <Text> Hello</Text>
-    </View>
-  );
-}
-
-export default ThirdScreenFunc; 
+ );
+};
+ 
+export default ThirdScreenFunc;
+ 
+ 
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   // justifyContent: 'flex-start',
+   marginHorizontal: 16,
+   marginVertical: 50,
+ },
+ head_title: {
+   //fontFamily: "SF Pro Display",
+   fontSize: 17,
+   textAlign: 'left',
+   opacity: 0.9,
+ },
+ title_bold: {
+   fontSize: 17,
+   textAlign: 'left',
+   opacity: 0.9,
+   fontWeight: 'bold',
+ },
+});
+ 
